457e13c27d5eea97afa74f2c0590369024c49e19
Aviv Cohen <a.b.cohen@wustl.edu>
2025-04-01 13:58:21 -0500

    fully functioning tic tac toe game that displays next player and the winner

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  
+
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -22,8 +24,17 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
