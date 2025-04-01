7354037278220931d164cd2a75bb6a2be9ce2257
Aviv Cohen <a.b.cohen@wustl.edu>
2025-04-01 14:01:18 -0500

    finished game function and removeed export default from Board function

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -55,7 +55,16 @@ export default function Board() {
 }
 
 export default function Game(){
-  
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
 }
 
 function calculateWinner(squares) {
